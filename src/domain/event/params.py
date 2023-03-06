325c82e0a17d398bbec4585e57b01e08ececab3f
Christopher Mckerracher <chrislmckerracher@gmail.com>
2023-03-06 09:47:52 -0500

    basic event param file started

---
+++ src/domain/event/params.py
@@ -0,0 +1,26 @@
+#This is a class of all event parameters
+#ToDo: rename thes params for their actual combat_loggy implementation(not that this should matter
+class Parameters:
+
+    timestamp = "timestamp"
+    sub_event = "sub_event"
+    hide_caster = "hide_caster"
+    source_guid = "source_guid"
+    source_name = "source_name"
+    source_flags = "source_flags"
+    source_raid_flags = "source_raid_flags"
+    dest_guid = "dest_guid"
+    dest_name = "dest_name"
+    dest_flags = "dest_flags"
+    dest_raid_flags = "dest_raid_flags"
+    amount = "amount"
+    overkill = "overkill"
+    school = "school"
+    resisted = "resisted"
+    blocked = "blocked"
+    absorbed = "absorbed"
+    critical = "critical"
+    glancing = "glancing"
+    crushing = "crushing"
+    is_off_hand = "is_off_hand"
+    
